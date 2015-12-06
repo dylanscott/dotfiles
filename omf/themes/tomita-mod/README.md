971fd3c072b21355cb85eebe71ff5dc89d27f23d
Dylan Scott <dylan.scott@gmail.com>
2015-12-05 18:09:37 -0800

    More informative git prompt

--- omf/themes/tomita-mod/README.md
+++ omf/themes/tomita-mod/README.md
@@ -3,3 +3,4 @@ a minimal fish shell theme, based on [tomita](https://github.com/daveyarwood/tom
 differences:
 * vi mode indicator compatible with fish_vi_mode instead of old omf vi-mode plugin
 * no newline in prompt
+* enables more informative git prompt
--- omf/themes/tomita-mod/fish_prompt.fish
+++ omf/themes/tomita-mod/fish_prompt.fish
@@ -1,3 +1,18 @@
+set -g __fish_git_prompt_show_informative_status true
+
+set -g __fish_git_prompt_char_stagedstate '±'
+set -g __fish_git_prompt_char_dirtystate '±'
+set -g __fish_git_prompt_char_untrackedfiles '+'
+set -g __fish_git_prompt_char_cleanstate '✓'
+
+set -g __fish_git_prompt_color_cleanstate green
+set -g __fish_git_prompt_color_stagedstate green
+set -g __fish_git_prompt_color_dirtystate red
+set -g __fish_git_prompt_color_untrackedfiles red
+set -g __fish_git_prompt_color_upstream_ahead green
+set -g __fish_git_prompt_color_upstream_behind red
+set -g __fish_git_prompt_color_invalidstate red
+
 function fish_prompt
   set_color $fish_color_cwd
   printf '%s' (prompt_pwd)
